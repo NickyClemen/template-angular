71150d8763c82a1133216957fda7058d4da41516
Nicole Ordoqui Morales <nicole.ordoqui@gmail.com>
2020-04-04 01:19:05 -0300

    Proyecto en fase 1 (agregado).

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { FlashMessagesModule } from 'angular2-flash-messages';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CarouselComponent } from './components/main/carousel/carousel.component';
+import { MainComponent } from './components/main/main.component';
+import { MainSectionComponent } from './components/main/main-section/main-section.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { SearchComponent } from './components/header/search/search.component';
+import { ItemComponent } from './components/item/item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavigationComponent,
+    FooterComponent,
+    CarouselComponent,
+    MainComponent,
+    MainSectionComponent,
+    LoginComponent,
+    RegisterComponent,
+    SearchComponent,
+    ItemComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule,
+    FlashMessagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
